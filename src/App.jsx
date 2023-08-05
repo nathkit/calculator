e5cf442a2b5c63f7b371126be0417c71ef6ff8df
Nathkit_Nae <nathkit101@gmail.com>
2023-08-06 00:44:32 +0700

    feat:add logic input number

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [output, setOutput] = useState(0);
 
   return (
     <>
@@ -12,47 +12,139 @@ function App() {
             <h1>asddasd</h1>
           </div>
           <div className="output-display">
-            <h1>sdfasf</h1>
+            <h1>{output}</h1>
           </div>
           <div className="btn-container">
             <div className="flex-wrap">
-              <button id="clear" className="basis-2/3" value="AC">
+              <button
+                id="clear"
+                className="basis-2/3"
+                value="AC"
+                onClick={() => setOutput(0)}>
                 AC
               </button>
-              <button id="divide" value="/">
+              <button id="divide" className="" value="/">
                 ÷
               </button>
-              <button id="seven" className="" value="7">
+              <button
+                id="seven"
+                className=""
+                value="7"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 7
               </button>
-              <button id="eight" className="" value="8">
+              <button
+                id="eight"
+                className=""
+                value="8"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 8
               </button>
-              <button id="nine" className="" value="9">
+              <button
+                id="nine"
+                className=""
+                value="9"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 9
               </button>
-              <button id="four" className="" value="4">
+              <button
+                id="four"
+                className=""
+                value="4"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 4
               </button>
-              <button id="five" className="" value="5">
+              <button
+                id="five"
+                className=""
+                value="5"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 5
               </button>
-              <button id="six" className="" value="6">
+              <button
+                id="six"
+                className=""
+                value="6"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 6
               </button>
-              <button id="one" className="" value="1">
+              <button
+                id="one"
+                className=""
+                value="1"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 1
               </button>
-              <button id="two" className="" value="2">
+              <button
+                id="two"
+                className=""
+                value="2"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 2
               </button>
-              <button id="three" className="" value="3">
+              <button
+                id="three"
+                className=""
+                value="3"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 3
               </button>
-              <button id="zero" className="basis-2/3" value="0">
+              <button
+                id="zero"
+                className="basis-2/3"
+                value="0"
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 0
               </button>
-              <button id="decimal" className="" value=".">
+              <button
+                id="decimal"
+                className=""
+                value="."
+                onClick={(e) => {
+                  setOutput(
+                    (!output || output === "0" ? "" : output) + e.target.value
+                  );
+                }}>
                 .
               </button>
             </div>
